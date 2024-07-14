69ef43d11818380a7315b0f76646dcaea6563045
Joshua Peralta <peralta.joshuapaul07@gmail.com>
2024-07-14 23:56:36 +1200

    generated new requirements.txt, linked my project to online postgre

--- Assignment1BE/settings.py
+++ Assignment1BE/settings.py
@@ -74,12 +74,22 @@ WSGI_APPLICATION = 'Assignment1BE.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': '0x1doazYvHjr',
+        'HOST': 'ep-yellow-queen-a4e2qzfc.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+    }}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
