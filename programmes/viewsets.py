8f053f081a1676eb2791c28dcc77411b41f6deeb
Joshua Peralta <peralta.joshuapaul07@gmail.com>
2024-07-15 19:47:12 +1200

    update serializers

--- programmes/viewsets.py
+++ programmes/viewsets.py
@@ -1,24 +1,29 @@
 from rest_framework import viewsets
 
 from programmes.models import Programme, Lecturer, Student, StudentMark, StudentAttendance
-from programmes.serializers import ProgrammeSerializer, LecturerSerializer, StudentSerializer, StudentMarkSerializer, StudentAttendanceSerializer
+from programmes.serializers import ProgrammeSerializer, LecturerSerializer, StudentSerializer, StudentMarkSerializer, \
+    StudentAttendanceSerializer
 
 
 class ProgrammeViewSet(viewsets.ModelViewSet):
     queryset = Programme.objects.all()
     serializer_class = ProgrammeSerializer
 
+
 class LecturerViewSet(viewsets.ModelViewSet):
     queryset = Lecturer.objects.all()
     serializer_class = LecturerSerializer
+
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+
 class StudentAttendanceViewSet(viewsets.ModelViewSet):
     queryset = StudentAttendance.objects.all()
     serializer_class = StudentAttendanceSerializer
+
 
 class StudentMarkViewSet(viewsets.ModelViewSet):
     queryset = StudentMark.objects.all()
