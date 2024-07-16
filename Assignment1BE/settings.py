851253a7cdca7f5427cb382de47804ff8954dc1b
Joshua Peralta <peralta.joshuapaul07@gmail.com>
2024-07-16 21:18:24 +1200

    add register path

--- Assignment1BE/settings.py
+++ Assignment1BE/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -159,3 +161,4 @@ CORS_ALLOWED_METHODS = [
 
     'PUT',
 ]
+
